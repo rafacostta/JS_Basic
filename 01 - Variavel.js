721bf62b078f6876550b70d48975d1450b693322
Rafael <rafael.costta13@outlook.com>
2024-02-23 12:01:02 -0300

    declarando variaveis

---
+++ 01 - Variavel.js
@@ -0,0 +1,15 @@
+/* VARIÁVEIS */
+
+// var
+    var nome = 'Rafael';
+// let
+    let sobreNome = 'Correa'
+// const
+    const IDADE = 26;
+
+    console.log(nome);
+
+    
+    console.log(sobreNome);
+    
+    console.log(IDADE);
