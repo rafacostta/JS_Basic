8475159ba74c72fef7449f1d1e4d88c13ebf8eba
Rafael <rafael.costta13@outlook.com>
2024-02-29 12:54:55 -0300

    Regras de nome de variável

---
+++ 03 - Regras para nome de variaveis.js
@@ -0,0 +1,28 @@
+/** REGRAS PARA NOME DE VARIÁVEIS */
+
+/** 
+ * 1 - Case-sensitive : Javascript Diferencia letras maiúsculas de minusculas,ou seja,
+ *  "nome", "NOME" ou "Nome" são variáveis diferentes.
+ * 
+ * 2 - Começar com letra, '$' ou '_', NÃO podendo começar com números ou outros caracteres.
+ * 
+ * 3 - Não usar palavras reservadas.
+ * * 
+*/
+
+/**
+ * CONVENÇÕES DE NOMECLATURA
+ * 
+ * 1 - Use camelCase para nomes de variável composto: 'nomeUsuario'.
+ * 
+ * 2 - Use letras maiúsculas para nomes de variaveis CONSTANTES: 'TAXA_JUROS'.
+ * 
+ */
+
+/** Exemplo de variáveis válidas: */
+var nome;
+var $precoProduto;
+let _qtdEstoque;
+let usuario1;
+var NomeCompleto;
+const TAXA_BASE = 0.1;
