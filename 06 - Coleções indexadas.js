c916979891320b8e2ee2742bf2a3b65dbeb3b282
Rafael Correa <rafael.costta13@gmail.com>
2024-03-08 13:24:31 -0300

    Coleções indexadas

---
+++ 06 - Coleções indexadas.js
@@ -0,0 +1,22 @@
+/** COLEÇÕES INDEXADAS */
+
+/** São coleções de dados indexados por um indice.
+ * São construtores de Arrays e array-like como objetos Array e typedArray.
+ */
+
+/** OBJETO ARRAY
+ * Conjunto de valores ordenados referenciado por um indice e um valor.
+ * Exemplo visual:
+ * [
+ *     INDEICE          VALOR     
+ *         1           Rafael
+ *         2           Correa
+ *         3           Ferreira
+ * ]
+ */
+
+/** Criando array */
+
+var array1 = new Array('elemento1', 'Elemento2');
+var array2 = Array('elemento1', 'Elemento2');
+var array3 = ['elemento1', 'Elemento2']; 
