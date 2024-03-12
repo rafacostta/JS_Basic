ef6e96d3d20c0f6af26778ae2a13b04abdfde2db
Rafael Correa <rafael.costta13@gmail.com>
2024-03-12 12:18:25 -0300

    Estrutura de dados

---
+++ 08 - Estrutura de dados.js
@@ -0,0 +1,28 @@
+/** ESTRUTURA DE DADOS */
+
+/** Estrutura de dados são maneiras de organizar e armazenar dados.
+ * Há várias estruturas de dados em Javascript, as mais comuns são:
+ */
+
+// Array - Coleção ordenada de elementos, onde os valores são acessados através dos indices.
+var numeros = [1,2,3,4,5];
+
+// Object - Coleção de pares Chave-Valor, onde os valores são acessados através das chaves únicas.
+var pessoa = {
+    nome: 'Rafael',
+    idade: 32,
+    apelido: 'Mr. Robot'
+};
+console.log(pessoa); // { nome: 'Rafael', idade: 32, apelido: 'Mr. Robot' }
+
+// Map - Coleção de pares chave-valor, onde a chave pode ser qualquer tipo de dado.
+var mapa = new Map();
+mapa.set("chave1", "valor-1");
+mapa.set("chave3", "valor-2");
+console.log(mapa); // Map { 'chave1' => 'valor-1', 'chave3' => 'valor-2' }
+
+// Set - Coleção de valores ÚNICOS, onde cada valor pode ocorrer apenas uma vez.
+var conjunto = new Set();
+conjunto.add(1);
+conjunto.add("Mr. Robot");
+console.log(conjunto);
