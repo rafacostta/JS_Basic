dc464c2a345097aff4ac0c5a92d4b07d83074d17
Rafael Correa <rafael.costta13@gmail.com>
2024-03-20 12:36:39 -0300

    Operadores Aritméticos.

---
+++ 16 - operadores aritmeticos.js
@@ -0,0 +1,33 @@
+// OPERADORES ARITMÉTICOS
+
+// (+) - Adição
+let a = 2 + 2;
+console.log(a); // 4
+
+// (-) - Subtração
+var b = 10 - 2;
+console.log(b); // 8
+
+// (*) - Multiplicação
+let c = 2 * 2;
+console.log(c); // 4
+
+// (/) - Divisão
+let d = 10 / 2;
+console.log(d); // 5
+
+// (%) - Módulo - calcula o resto da divisão entre dois valores.
+let e = 10 % 2; 
+console.log(e); // Resultado 0
+let f = 7 % 3;
+console.log(f);  // Resultado 1
+
+// (++) - Incremento, soma um ao valor de uma variável.
+let g = 1;
+g++;
+console.log("Incrementa + 1 : " + g); // 2
+
+// (--) - Decremento, Subtrai 1 da variável.
+let h = 10;
+h--;
+console.log("Decremento : " + h); //9
