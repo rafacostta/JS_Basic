7471ad6dbdf022719b3115d4468629a94eb3ca0c
Rafael Correa <rafael.costta13@gmail.com>
2024-03-21 12:04:46 -0300

    Operadores de vítgula

---
+++ 21 - operadores de virgula.js
@@ -0,0 +1,11 @@
+/** OPERADORES DE VÍRGULA
+ * Usado para avaliar expressões em sequência e retorna o valor da última expressão.
+ * Usado quando precisa avaliar várias expressões e apenas o resultado
+ * da última é importante.
+ * 
+ */
+
+var x = (2+2, 3+3, 4+4);
+console.log(x);// 8
+
+
