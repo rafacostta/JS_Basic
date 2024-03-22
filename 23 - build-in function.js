7da68f9fa7eccd73f4b5506c0ba4cc019f91145b
Rafael Correa <rafael.costta13@gmail.com>
2024-03-22 12:15:27 -0300

    build-in function

---
+++ 23 - build-in function.js
@@ -0,0 +1,8 @@
+/** BUILD-IN FUNCTION
+ * São funções fornecidas pelo próprio ambiente javascript.
+ * São funções fornecidas pela própria linguagem.
+ * Estão disponíveis para uso imediato sem necessidade de importar explicitamente.
+ * 
+ */
+
+// Vamos explorando aos poucos essas funcções ...
